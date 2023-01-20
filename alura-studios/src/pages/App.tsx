a40090a615838027c626b1ae3a0030d08a9e00f3
JacksonSM <jacksons.m2018@gmail.com>
2023-01-20 17:38:42 -0300

    initial

---
+++ alura-studios/src/pages/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Formulario from '../components/Formulario'
+import Lista from '../components/Lista'
+import './style.scss';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="AppStyle">
+      <Formulario/>
+      <Lista/>
+    </div>
+  )
+}
+
+export default App;
